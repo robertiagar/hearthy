d84c8e032df8b6e5c531441253f969b3474d4e59
Garjy <chrisss.g@gmail.com>
2015-06-27 14:59:31 +0300

    Adding graphs functionality

--- mobile/src/main/java/ro/multiplesingleton/hearthy/MainActivity.java
+++ mobile/src/main/java/ro/multiplesingleton/hearthy/MainActivity.java
@@ -1,9 +1,18 @@
 package ro.multiplesingleton.hearthy;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.echo.holographlibrary.Line;
+import com.echo.holographlibrary.LinePoint;
+import com.echo.holographlibrary.LineGraph;
+
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +21,43 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Line l = new Line();
+        LinePoint p = new LinePoint();
+        p.setX(0);
+        p.setY(5);
+        l.addPoint(p);
+        p = new LinePoint();
+        p.setX(8);
+        p.setY(8);
+        l.addPoint(p);
+        p = new LinePoint();
+        p.setX(10);
+        p.setY(4);
+        l.addPoint(p);
+        l.setColor(Color.parseColor("#FFBB33"));
+
+        final LineGraph li = (LineGraph)findViewById(R.id.graph);
+        li.addLine(l);
+        li.setRangeY(0, 10);
+        li.setLineToFill(0);
+
+        Button button = (Button)findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random random = new Random();
+                Line line = li.getLine(0);
+                Float f1 = GetFloat(10,20);
+                float f2 = GetFloat(20,30);
+                line.addPoint(new LinePoint());
+                li.addLine(line);
+            }
+        });
+    }
+
+    private float GetFloat(float x,float y){
+        Random rand = new Random();
+        return rand.nextFloat() * (x - y) + x;
     }
 
     @Override
